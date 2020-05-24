901ae6c1510b4566f8b99b40a0960bb6b0f945d6
HubertC1 <51125139+HubertC1@users.noreply.github.com>
2020-05-24 13:10:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,43 @@
-#include <iostream>
+#include<iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+int tests;
+int pers;
+vector<int> inexp;
+int input;
+int curr;
+int groups;
+int remain;
+
 int main(){
-    int w;
-    cin >> w;
-    if (w%2==0){
-        cout << "YES";
-    }else{
-        cout << "NO";
+    cin >> tests;
+    for (int i = 0; i<tests; i++){
+        groups = 0;
+        remain = 0;
+        inexp.clear();
+        cin >> pers;
+        for (int j=0; j<pers; j++){
+            cin >> input;
+            inexp.push_back(input);
+        }
+        sort(inexp.begin(),inexp.end());
+        for (int a=0; a<pers; a+=0){
+            curr = inexp[a];
+            if(curr==inexp[a+curr-1] && a+curr <= inexp.size()){
+                groups ++;
+                a += curr;
+            }else if(remain + 1 == curr){
+                remain = 0;
+                groups ++;
+                a ++;
+            }else{
+                remain ++;
+                a ++;
+            }
+        }
+        cout << groups << endl;
     }
     return 0;
 }
